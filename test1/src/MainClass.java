6057eb44c849d47d93a6bf5759289317ed434f78
smoleo <LeonS1352@gmail.cpm>
2020-07-14 11:11:37 +0200

    test text wurde geändert

--- test1/src/MainClass.java
+++ test1/src/MainClass.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class MainClass {
     public static void main(String[] args) {
         int test = 0;
+        int l = 0;
+        Scanner scan = new Scanner(System.in);
         test++;
         System.out.println(test);
 
-        for (int idx = 0; idx < 50; idx++) {
+        System.out.println("gib eine zahl ein");
+        l=scan.nextInt();
+        for (int idx = 0; idx < l; idx++) {
             for (int i = 0; i <= idx; i++) {
                 if (i % 10 == 0) {
                     System.out.print("[" + i + "],");
