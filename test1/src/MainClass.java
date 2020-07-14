21ad211e2e3583686acc66900d35db7b4a85bdf0
smoleo <LeonS1352@gmail.cpm>
2020-07-14 11:06:45 +0200

    test2

--- test1/src/MainClass.java
+++ test1/src/MainClass.java
@@ -1,15 +1,19 @@
 public class MainClass {
     public static void main(String[] args) {
-        int test=0;
+        int test = 0;
         test++;
         System.out.println(test);
 
-        for(int idx = 0; idx < 40; idx++) {
-            for(int i=0;i<=idx;i++)
-            {
-                System.out.print(i+",");
+        for (int idx = 0; idx < 50; idx++) {
+            for (int i = 0; i <= idx; i++) {
+                if (i % 10 == 0) {
+                    System.out.print("[" + i + "],");
+                } else {
+                    System.out.print(i + ",");
+                }
             }
             System.out.println(" ");
         }
+
     }
 }
