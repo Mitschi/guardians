c96421fd1dd178c58463756cf8d380c5371537bd
simonambrosch <simon.ambrosch@gmail.com>
2020-07-20 14:56:21 +0200

    Initial Commit

---
+++ src/main/java/com/github/mitschi/guardiansweb/GuardiansWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.mitschi.guardiansweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardiansWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardiansWebApplication.class, args);
+	}
+
+}
