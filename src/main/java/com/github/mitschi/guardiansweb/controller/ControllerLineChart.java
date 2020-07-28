30dc678b978e574f9adf0ffaa1ac752e1b14308c
smoleo <LeonS1352@gmail.cpm>
2020-07-28 08:44:07 +0200

    data for all charts comes from database

--- src/main/java/com/github/mitschi/guardiansweb/controller/ControllerLineChart.java
+++ src/main/java/com/github/mitschi/guardiansweb/controller/ControllerLineChart.java
@@ -14,7 +14,7 @@ public class ControllerLineChart {
     @RequestMapping("/lineChart")
     public String index() throws Exception {
         String filePath = "src/main/resources/HTML/lineChart.html";
-        String data= H2Read.H2read("select * from date_value","date","value");
+        String data= H2Read.H2read("select * from date_value order by date","date","value");
         String[][] separatedValues= FileHandler.convertStringTo2DArray(data);
 
         String[] columns = new String[] {"date", "value"};
