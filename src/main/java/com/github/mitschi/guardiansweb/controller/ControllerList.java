b9510bcb0b0fd624ef5e44e8ebaa24b3bf3773c6
simonambrosch <simon.ambrosch@gmail.com>
2020-08-03 10:11:41 +0200

    Nothing changed

--- src/main/java/com/github/mitschi/guardiansweb/controller/ControllerList.java
+++ src/main/java/com/github/mitschi/guardiansweb/controller/ControllerList.java
@@ -77,8 +77,10 @@ public class ControllerList {
     @GetMapping("/list")
     public String listForm(Model model) throws Exception {
         model.addAttribute("tableEntry", new TableEntry());
+
         String htmlText=index();
-        FileHandler.writeToFile("src/main/resources/templates/list.html",htmlText);
+        FileHandler.writeToFile("src/main/resources/templates/list.html", htmlText);
+
         return "list";
     }
 }
